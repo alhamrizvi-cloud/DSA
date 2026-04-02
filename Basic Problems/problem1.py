86f7d4b6426e6b412ae7c1d5bc4efc6ebb90f73d
𝐀𝐥𝐡𝐚𝐦 𝐑𝐢𝐳𝐯𝐢 <its47h4m@gmail.com>
2026-04-03 01:05:05 +0530

    Extraction of Digits Using Loops
    Updated the loop to include comments for clarity.

--- Basic Problems/problem1.py
+++ Basic Problems/problem1.py
@@ -1,6 +1,14 @@
+# Write a program to extract and print all digits of a number using a loop.
+
 n = 5873
 
 while n > 0:
-    digit = n % 10
-    print(digit)
-    n = n // 10
+    digit = n % 10   # get last digit
+    print(digit) 
+    n = n // 10      # remove last digit from number
+
+# Output:
+# 3
+# 7
+# 8
+# 5
