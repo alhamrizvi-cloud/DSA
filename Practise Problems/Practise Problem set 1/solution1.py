973622661f32024d7d996c329817029f668d927f
𝐀𝐥𝐡𝐚𝐦 𝐑𝐢𝐳𝐯𝐢 <its47h4m@gmail.com>
2026-04-04 01:36:40 +0530

    Update solution1.py

--- Practise Problems/Practise Problem set 1/solution1.py
+++ Practise Problems/Practise Problem set 1/solution1.py
@@ -3,9 +3,10 @@ x = 5873
 while x > 0:
   last_digit = x % 10
   #which will give us input 3
+  total += last_digit
   x = x // 10
   # which will return 5873 to 587 to 58..
-  total += last_digit
+  
 return total
 print(sum_of_digits(5873))  # Output: 23
   
