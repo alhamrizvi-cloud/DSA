5930acdfc714f9169f2e2961e90a35161a35cb40
𝐀𝐥𝐡𝐚𝐦 𝐑𝐢𝐳𝐯𝐢 <its47h4m@gmail.com>
2026-04-04 01:39:15 +0530

    Sum of Digits n = 5873

--- Practise Problems/Practise Problem set 1/solution1.py
+++ Practise Problems/Practise Problem set 1/solution1.py
@@ -1,12 +1,10 @@
 x = 5873
 total = 0
+
 while x > 0:
-  last_digit = x % 10
-  #which will give us input 3
-  total += last_digit
-  x = x // 10
-  # which will return 5873 to 587 to 58..
-  
-return total
-print(sum_of_digits(5873))  # Output: 23
+    last_digit = x % 10
+    total += last_digit
+    x = x // 10
+
+print(total)  # ✅ Output: 23
   
